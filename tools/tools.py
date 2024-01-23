151270cb45107d641e6ca44b48c90394c4d26474
botdenoms <dudubot404@gmail.com>
2024-01-23 19:44:26 +0300

    tools: tools module/package functions declarations without implementations

--- tools/tools.py
+++ tools/tools.py
@@ -1,1 +1,35 @@
+# imports section
 
+
+# functions
+def get_site(url=''):
+    "returns a response object from the given url parameter or None if not found/error"
+    return None
+
+def get_domain_name(url=''):
+    "returns a string containing the domain name of the given url"
+    return None
+
+def get_links(url=''):
+    "returns a list of unqiue url links from the given page"
+    return []
+
+def get_internal_links(url=''):
+    "returns a list with unqiue url links from the given page within its domain"
+    return []
+
+def get_external_links(url=''):
+    "returns a list with unqiue url links from the given page outside its domain"
+    return []
+
+def get_images(url=''):
+    "returns a list of unique image urls from the given page"
+    return []
+
+def get_emails(url=''):
+    "returns a list of unique email address present at the given url"
+    return []
+
+def get_videos(url=''):
+    "returns a list of unique video urls from the given page"
+    return []
