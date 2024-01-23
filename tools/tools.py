19e448b30a1b0cb6df3c03558de4b11c6788e2b8
botdenoms <dudubot404@gmail.com>
2024-01-23 20:09:36 +0300

    tools: tools module/package more functions added without implementations

--- tools/tools.py
+++ tools/tools.py
@@ -33,3 +33,54 @@ def get_emails(url=''):
 def get_videos(url=''):
     "returns a list of unique video urls from the given page"
     return []
+
+def domain_bfs_crawl(homeurl=''):
+    """
+    using breath first search, crawls the given url as the home url
+    returns a list with unqiue url links within the domain of the url
+    """
+    return []
+
+def limited_domain_bfs_crawl(homeurl='', limit=5000):
+    """
+    using breath first search, crawls the given url as the home url \n
+    crawling is limited by a limit which will not be exceeded if more links are present\n 
+    returns a list with unqiue url links within the domain of the url
+    """
+    return []
+
+def domain_dfs_crawl(homeurl=''):
+    """
+    using depth first search, crawls the given url as the home url
+    returns a list with unqiue url links within the domain of the url
+    """
+    return []
+
+def limited_domain_dfs_crawl(homeurl='', limit=5000):
+    """
+    using depth first search, crawls the given url as the home url \n
+    crawling is limited by a limit which will not be exceeded if more links are present\n 
+    returns a list with unqiue url links within the domain of the url
+    """
+    return []
+
+def email_extractor(url='', limit=20):
+    """
+    crawls the given url plus its links limited by the limit depth \n
+    returns a list with unqiue email address present within the domain of the url
+    """
+    return []
+
+def phone_extractor(url='', limit=20):
+    """
+    crawls the given url plus its links limited by the limit depth \n
+    returns a list with unqiue phone numbers present within the domain of the url
+    """
+    return []
+
+def socials_extractor(url='', limit=20):
+    """
+    crawls the given url plus its links limited by the limit depth \n
+    returns a list with unqiue social media urls present within the domain of the url
+    """
+    return []
